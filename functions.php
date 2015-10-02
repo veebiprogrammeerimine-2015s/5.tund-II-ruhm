2e89dcd833771649d0b16896753c70888ef30f61
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-02 11:09:52 +0300

    separated functionality from login to functions

--- functions.php
+++ functions.php
@@ -1,20 +1,41 @@
 <?php
 	//kõik AB'iga seonduv
 	
-	function welcomeUser($name, $age){
-		echo "Tere ".$name.", kelle vanus on ".$age."<br>";
-		echo $first_name;
+	// ühenduse loomiseks kasuta
+	require_once("../configglobal.php");
+	$database = "if15_romil_2";
+
+	
+	// lisame kasutaja ab'i
+	function createUser(){
+		
+		$mysqli = new mysqli($servername, $server_username, $server_password, $database);
+		
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+		$stmt->bind_param("ss", $create_email, $password_hash);
+		$stmt->execute();
+		$stmt->close();
+		
+		$mysqli->close();		
 	}
 	
-	echo $age;
-	
-	$first_name = "juku";
-	welcomeUser($first_name, 1);
-	
-	$first_name = "Juhan";
-	welcomeUser($first_name, 10);
-	
-	welcomeUser("Romil", 5);
-	
+	//logime sisse
+	function loginUser(){
+		
+		$mysqli = new mysqli($servername, $server_username, $server_password, $database);
+		
+		$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+		$stmt->bind_param("ss", $email, $password_hash);
+		$stmt->bind_result($id_from_db, $email_from_db);
+		$stmt->execute();
+		if($stmt->fetch()){
+			echo "kasutaja id=".$id_from_db;
+		}else{
+			echo "Wrong password or email!";
+		}
+		$stmt->close();
+		
+		$mysqli->close();
+	}
 	
 ?>
--- login.php
+++ login.php
@@ -1,10 +1,5 @@
 <?php
 	
-	// ühenduse loomiseks kasuta
-	require_once("../config.php");
-	$database = "if15_romil_2";
-	$mysqli = new mysqli($servername, $username, $password, $database);
-
   // muuutujad errorite jaoks
 	$email_error = "";
 	$password_error = "";
@@ -44,24 +39,8 @@
 				
 				$password_hash = hash("sha512", $password);
 				
-				$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
-				$stmt->bind_param("ss", $email, $password_hash);
-				
-				//paneme vastuse muutujatesse
-				$stmt->bind_result($id_from_db, $email_from_db);
-				$stmt->execute();
-				
-				//küsima kas AB'ist saime kätte
-				if($stmt->fetch()){
-					//leidis
-					echo "kasutaja id=".$id_from_db;
-				}else{
-					// tühi, ei leidnud , ju siis midagi valesti
-					echo "Wrong password or email!";
-					
-				}
-				
-				$stmt->close();
+				// functions php failis käivitan funktsiooni
+				loginUser();
 			}
 
 		} // login if end
@@ -94,16 +73,8 @@
 				echo "<br>";
 				echo $password_hash;
 				
-				$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
-				
-				//echo $mysqli->error;
-				//echo $stmt->error;
-				//asendame ? märgid muutujate väärtuste
-				// ss - s tähendab string iga muutuja kohta
-				$stmt->bind_param("ss", $create_email, $password_hash);
-				$stmt->execute();
-				$stmt->close();
-				
+				// functions.php failis käivina funktsiooni
+				createUser();
 				
 			}
 
@@ -119,8 +90,7 @@
   	return $data;
   }
   
-  // paneme ühenduse kinni
-  $mysqli->close();
+  
 
 ?>
 <!DOCTYPE html>
