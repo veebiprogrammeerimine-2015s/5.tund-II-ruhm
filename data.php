913afaf514026950351b144c39b0f10f061d8804
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-02 13:13:03 +0300

    create car plate form

--- data.php
+++ data.php
@@ -17,9 +17,22 @@
 		header("Location: login.php");
 		
 	}
+	
+	$car_plate = $color = $car_plate_error = $color_error = "";
+	
+	
 ?>
 
 <p>
 	Tere, <?=$_SESSION["user_email"];?>
 	<a href="?logout=1"> Logi välja</a>
 </p>
+
+ <h2>Lisa auto</h2>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+  	<label for="car_plate" >auto nr</label><br>
+	<input id="car_plate" name="car_plate" type="text" value="<?=$car_plate; ?>"> <?=$car_plate_error; ?><br><br>
+  	<label>värv</label><br>
+	<input name="color" type="text" value="<?=$color; ?>"> <?=$color_error; ?><br><br>
+  	<input type="submit" name="create" value="Salvesta">
+  </form>
