51b49c968f2ff839c7fc7ecc43d3aabda877a92f
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-02 12:59:50 +0300

    session and log out

--- data.php
+++ data.php
@@ -8,6 +8,18 @@
 		// suunan login lehele
 		header("Location: login.php");
 	}
+	
+	//login välja, aadressireal on ?logout=1
+	if(isset($_GET["logout"])){
+		//kustutab kõik sessiooni muutujad
+		session_destroy();
+		
+		header("Location: login.php");
+		
+	}
 ?>
 
-data
+<p>
+	Tere, <?=$_SESSION["user_email"];?>
+	<a href="?logout=1"> Logi välja</a>
+</p>
