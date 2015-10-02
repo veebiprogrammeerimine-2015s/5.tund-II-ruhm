fdfe4f57f59655ea720bb18fcc290f298021f023
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-02 10:51:21 +0300

    fn example

--- functions.php
+++ functions.php
@@ -1,3 +1,20 @@
 <?php
 	//kõik AB'iga seonduv
+	
+	function welcomeUser($name, $age){
+		echo "Tere ".$name.", kelle vanus on ".$age."<br>";
+		echo $first_name;
+	}
+	
+	echo $age;
+	
+	$first_name = "juku";
+	welcomeUser($first_name, 1);
+	
+	$first_name = "Juhan";
+	welcomeUser($first_name, 10);
+	
+	welcomeUser("Romil", 5);
+	
+	
 ?>
